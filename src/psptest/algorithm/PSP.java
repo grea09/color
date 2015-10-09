469004d1ff67643b1def9dee0b1043bf7f6d3983
Antoine GRÉA <antoine@grea.me>
2015-10-09 17:24:10 +0200

    * Refactor pour rendre le code plus clair et plus extenssiblee * recherche de chemin résolveur avant confirmation de menace * Optimisation (on ne cherche que le premier sous-but ou menace)

--- src/psptest/algorithm/PSP.java
+++ src/psptest/algorithm/PSP.java
@@ -10,17 +10,18 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import me.grea.antoine.log.Log;
-import org.jgrapht.DirectedGraph;
 import org.jgrapht.alg.CycleDetector;
-import org.jgrapht.graph.DefaultEdge;
 import psptest.type.Action;
 import psptest.type.Edge;
 import psptest.exception.Failure;
 import psptest.type.Problem;
 import psptest.exception.Success;
+import psptest.type.Problem.SubGoal;
+import psptest.type.Problem.Threat;
 
 /**
  *
@@ -37,7 +38,7 @@ public class PSP {
             throw new Failure(null);
         } catch (Success s) {
             //TODO add plan building
-            s.printStackTrace(Log.out);
+            Log.v(s);
         }
     }
 
@@ -48,121 +49,79 @@ public class PSP {
             throw new Success();
         } catch (Failure ex) {
             Log.e(ex);
-            return;
         }
     }
 
     private static void satisfy(Problem problem) throws Success, Failure {
-        Map<Integer, Action> subgoals = subgoal(problem);
-        Log.d("Subgoals : " + subgoals);
-        for (Map.Entry<Integer, Action> subgoal : subgoals.entrySet()) {
-            if (problem.initial.effects.contains(subgoal.getKey())) {
-                insert(problem, subgoal.getKey(), subgoal.getValue(), problem.initial);
+//        Set<SubGoal> subgoals = problem.suboals();
+//        Log.d("Subgoals : " + subgoals);
+        SubGoal subGoal = problem.findSubGoal();
+        if (subGoal != null) {
+            if (problem.initial.effects.contains(subGoal.subgoal)) {
+                insert(problem, subGoal, problem.initial);
             }
 
             for (Action step : problem.plan.vertexSet()) {
-                if (step.effects.contains(subgoal.getKey())) {
-                    insert(problem, subgoal.getKey(), subgoal.getValue(), step);
+                if (step.effects.contains(subGoal.subgoal)) {
+                    insert(problem, subGoal, step);
                 }
             }
             for (Action action : problem.actions) {
-                if (action.effects.contains(subgoal.getKey())) {
-                    insert(problem, subgoal.getKey(), subgoal.getValue(), action);
+                if (action.effects.contains(subGoal.subgoal)) {
+                    insert(problem, subGoal, action);
                 }
             }
-            throw new Failure((Object) subgoal);
+            throw new Failure(subGoal);
         }
+        //Success as we return
     }
 
-    private static Map<Integer, Action> subgoal(Problem problem) {
-        Map<Integer, Action> subgoals = new HashMap<>();
-        Deque<Action> open = new ArrayDeque<>(Arrays.asList(problem.goal));
-        while (!open.isEmpty()) {
-            Action current = open.pollLast();
-//            for (int effect : current.effects) {
-//                subgoals.remove(effect);
-//            }
-            for (int precondition : current.preconditions) {
-//                if (!problem.initial.effects.contains(precondition)) {
-                subgoals.put(precondition, current);
-//                }
-            }
-            for (Edge edge : problem.plan.incomingEdgesOf(current)) {
-                subgoals.remove(edge.label);
-                open.addFirst(problem.plan.getEdgeSource(edge));
-            }
-        }
-        return subgoals;
-    }
+    private static void insert(Problem problem, SubGoal subGoal, Action candidate) throws Success {
 
-    private static void insert(Problem problem, int subgoal, Action toSatisfy, Action candidate) throws Success {
-        Log.d("Inserting " + candidate + " to satisfy precondition " + subgoal + " of goal " + toSatisfy);
         //TODO Fail if negative effects
-        problem.plan.addVertex(candidate);
-        Edge edge = problem.plan.addEdge(candidate, toSatisfy);
-        edge.label = subgoal;
+        subGoal.satisfy(candidate);
 
         solve_(problem);
 
-        Log.d("Action " + candidate + " not suited, reverting");
-        problem.plan.removeEdge(candidate, toSatisfy);
-        if (problem.plan.outDegreeOf(candidate) == 0) {
-            problem.plan.removeVertex(candidate);
-        }
+        subGoal.revert();
     }
 
     private static void unthreaten(Problem problem) throws Success, Failure { //TODO ignore initial and goal
-        for (Action candidate : problem.plan.vertexSet()) {
-            for (int effect : candidate.effects) {
-                for (Edge oposite : problem.plan.edgeSet()) {
-                    if ((Integer) oposite.label == -effect) { //NEVER have a 0 effect
-                        Action source = problem.plan.getEdgeSource(oposite);
-                        Action target = problem.plan.getEdgeTarget(oposite);
-                        if (problem.plan.getEdge(candidate, source) == null
-                                && problem.plan.getEdge(target, candidate) == null) {
-                            Log.w("" + candidate + " is a threat to the link " + source + " => " + target);
-                            demote(problem, source, target, candidate, effect);
-                            promote(problem, source, target, candidate, effect);
-                            throw new Failure(candidate);
-                        }
-                    }
-                }
-            }
+        Threat threat = problem.findThreat();
+        if (threat != null) {
+            Log.w("Found a threat " + threat);
+            demote(problem, threat);
+            promote(problem, threat);
+            throw new Failure(threat);
         }
     }
 
-    private static void demote(Problem problem, Action source, Action target, Action threat, int effect) throws Success {
-        Log.d("Demoting " + threat + " in the link " + problem.plan.getEdge(source, target));
-        if (target == problem.goal) {
-            Log.w("Can't demote after goal step !");
+    private static void demote(Problem problem, Threat threat) throws Success {
+        try {
+            threat.demote();
+        } catch (Failure ex) {
             return;
         }
-        Edge edge;
-        edge = problem.plan.addEdge(target, threat);
-        edge.label = 0;
-        // TODO consistency check
-        if (!new CycleDetector<>(problem.plan).detectCycles()) {
+
+        if (problem.consistent()) {
             solve_(problem);
         }
-        // Revert !
-        problem.plan.removeEdge(edge);
+
+        threat.undemote();
     }
 
-    private static void promote(Problem problem, Action source, Action target, Action threat, int effect) throws Success {
-        Log.d("Promoting " + threat + " in the link " + problem.plan.getEdge(source, target));
-        if (source == problem.initial) {
-            Log.w("Can't promote before initial step !");
+    private static void promote(Problem problem, Threat threat) throws Success {
+        try {
+            threat.promote();
+        } catch (Failure ex) {
             return;
         }
-        Edge edge;
-        edge = problem.plan.addEdge(threat, source);
-        edge.label = 0;
-        // TODO consistency check
-        if (!new CycleDetector<Action, Edge>(problem.plan).detectCycles()) {
+
+        if (problem.consistent()) {
             solve_(problem);
         }
-        // Revert !
-        problem.plan.removeEdge(edge);
+
+        threat.unpromote();
     }
 
 }
--- src/psptest/type/Problem.java
+++ src/psptest/type/Problem.java
@@ -5,17 +5,25 @@
  */
 package psptest.type;
 
-import psptest.type.Goal;
-import psptest.type.Edge;
-import psptest.type.Action;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import me.grea.antoine.log.Log;
 import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.alg.DijkstraShortestPath;
+import psptest.exception.Failure;
 
 /**
  *
  * @author antoine
  */
 public class Problem {
+
     public Action initial;
     public Action goal;
     public Set<Action> actions; // not including those above
@@ -28,22 +36,192 @@ public class Problem {
         this.plan = plan;
     }
 
+    public class SubGoal {
+
+        public Action needer;
+        public int subgoal;
+        private Edge causal;
+        private Action provider;
+
+        public SubGoal(Action needer, int subgoal) {
+            this.needer = needer;
+            this.subgoal = subgoal;
+        }
+
+        public void satisfy(Action provider) {
+            this.provider = provider;
+            plan.addVertex(provider);
+            causal = plan.addEdge(provider, needer);
+            causal.label = subgoal;
+            Log.d("Inserting " + this);
+        }
+
+        public void revert() {
+            Log.w("Action " + provider + " not suited to achieve " + subgoal + " of " + needer + " ! Reverting");
+            plan.removeEdge(causal);
+            if (plan.outDegreeOf(provider) == 0) {
+                plan.removeVertex(provider);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return (provider != null && plan.containsVertex(provider) ? provider : "?")
+                    + " =[" + subgoal + "]>" + needer;
+        }
+
+    }
+
+    public Set<SubGoal> suboals() {
+        Map<Integer, SubGoal> subgoals = new HashMap<>();
+        Deque<Action> open = new ArrayDeque<>(Arrays.asList(goal));
+        while (!open.isEmpty()) {
+            Action current = open.pollLast();
+//            for (int effect : current.effects) {
+//                subgoals.remove(effect);
+//            }
+            for (int precondition : current.preconditions) {
+//                if (!problem.initial.effects.contains(precondition)) {
+                subgoals.put(precondition, new SubGoal(current, precondition));
+//                }
+            }
+            for (Edge edge : plan.incomingEdgesOf(current)) {
+                subgoals.remove(edge.label);
+                open.addFirst(plan.getEdgeSource(edge));
+            }
+        }
+        return new HashSet<>(subgoals.values());
+    }
+
+    public SubGoal findSubGoal() {
+        Deque<Action> open = new ArrayDeque<>(Arrays.asList(goal));
+        while (!open.isEmpty()) {
+            Action current = open.pollLast();
+            for (int precondition : current.preconditions) {
+                boolean satisfied = false;
+                for (Edge edge : plan.incomingEdgesOf(current)) {
+                    if ((Integer) edge.label == precondition) {
+                        satisfied = true;
+                    }
+                    open.addFirst(plan.getEdgeSource(edge));
+                }
+                if (!satisfied) {
+                    return new SubGoal(current, precondition);
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Threat {
+
+        public Action threat;
+        public Edge threatened;
+        private Edge demoter;
+        private Edge promoter;
+
+        public Threat(Action threat, Edge threatened) {
+            this.threat = threat;
+            this.threatened = threatened;
+        }
+
+        public void demote() throws Failure {
+            Log.d("Demoting " + threat + " for the link " + threatened);
+            if (plan.getEdgeTarget(threatened) == goal) {
+                Log.w("Can't demote after goal step !");
+                throw new Failure(this);
+            }
+            demoter = plan.addEdge(plan.getEdgeTarget(threatened), threat);
+            demoter.label = 0;
+        }
+
+        public void undemote() {
+            plan.removeEdge(demoter);
+        }
+
+        public void promote() throws Failure {
+            Log.d("Promoting " + threat + " for the link " + threatened);
+            if (plan.getEdgeSource(threatened) == initial) {
+                Log.w("Can't promote before initial step !");
+                throw new Failure(this);
+            }
+            promoter = plan.addEdge(threat, plan.getEdgeSource(threatened));
+            promoter.label = 0;
+        }
+
+        public void unpromote() {
+            plan.removeEdge(promoter);
+        }
+
+        @Override
+        public String toString() {
+            return threat + " ☠ " + threatened;
+        }
+
+    }
+
+    public Set<Threat> threats() {
+        Set<Threat> threats = new HashSet<>();
+        for (Action candidate : plan.vertexSet()) {
+            for (int effect : candidate.effects) {
+                for (Edge oposite : plan.edgeSet()) {
+                    if ((Integer) oposite.label == -effect) { //NEVER have a 0 effect
+                        Action source = plan.getEdgeSource(oposite);
+                        Action target = plan.getEdgeTarget(oposite);
+//                        if (!plan.containsEdge(candidate, source)
+//                                && !plan.containsEdge(target, candidate)) {
+                        if (!pathExists(candidate, source) && !pathExists(target, candidate)) {
+                            threats.add(new Threat(candidate, oposite));
+                        }
+//                        }
+                    }
+                }
+            }
+        }
+        return threats;
+    }
+
+    public Threat findThreat() {
+        for (Action candidate : plan.vertexSet()) {
+            for (int effect : candidate.effects) {
+                for (Edge oposite : plan.edgeSet()) {
+                    if ((Integer) oposite.label == -effect) { //NEVER have a 0 effect
+                        Action source = plan.getEdgeSource(oposite);
+                        Action target = plan.getEdgeTarget(oposite);
+//                        if (!plan.containsEdge(candidate, source)
+//                                && !plan.containsEdge(target, candidate)) {
+                        if (!pathExists(candidate, source) && !pathExists(target, candidate)) {
+                            return new Threat(candidate, oposite);
+                        }
+//                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public boolean consistent() {
+        return !(new CycleDetector<>(plan).detectCycles());
+    }
+
+    public boolean pathExists(Action source, Action target) {
+        return new DijkstraShortestPath<>(plan, source, target).getPathLength() != Double.POSITIVE_INFINITY;
+    }
+
     @Override
     public String toString() {
-        return "Problem {" + "\n\tinitial:" + initial.effects + " => goal:" + ((Goal)goal).toString() + "\n\tactions:" + actions + "\n\tplan:" + planToString() + "}";
+        return "Problem {" + "\n\tinitial:" + initial.effects + " => goal:" + ((Goal) goal).toString() + "\n\tactions:" + actions + "\n\tplan:" + planToString() + "}";
     }
-    
-    public String planToString()
-    {
+
+    public String planToString() {
 //        return plan.edgeSet().toString();
         String result = "";
-        for(Edge edge : plan.edgeSet())
-        {
-            result += edge +"\n\t";
+        for (Edge edge : plan.edgeSet()) {
+            result += edge + "\n\t";
         }
         return result;
     }
-    
-    
-    
+
 }
